f07ed66aaa711c1d81c87705489e9587526f83b3
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-03-17 04:01:10 -0700

    Renamed quiz1 to Poly

--- Poly.hs
+++ Poly.hs
@@ -1,21 +1,13 @@
-module Quiz1 where
+module Poly where
+
 newtype Poly = Poly [Int]
 
-{- | Embed a list of integers into a mapped type
-   | to treat them as coefficients on polynomial terms
--}
+-- | Embed a list of integers into a mapped type
+--   | to treat them as coefficients on polynomial terms
 toPoly :: [Int] -> Poly
 toPoly = Poly
 
 instance Show Poly where
-
-  {- | Renders full polynomial term given coefficents
-    |
-    | > toPoly [2,-3,0,7]
-    | 2x^3 - 3x^2 + 7x^0
-    | > toPoly [-5, -3, 7, 0, -1, 3]
-    | -5x^5 - 3x^4 + 7x^3 - 1x^1 + 3x^0
-  -}
   show (Poly xs) =
     leadingPolarity $ snd (foldr renderTerm (0, "") xs)
     where
@@ -42,30 +34,28 @@ instance Show Poly where
             | coeff == 0 = ""
             | otherwise = " - " ++ drop 2 term
 
-{- | Add 2 polynomials and re
-   |
-   | addPoly (toPoly [1, 2, 3]) (toPoly [10, 20, 30])
-   | > 11x^2 + 22x^1 + 33x^0
--}
+-- | Add 2 polynomials and re
+--   |
+--   | addPoly (toPoly [1, 2, 3]) (toPoly [10, 20, 30])
+--   | > 11x^2 + 22x^1 + 33x^0
 addPoly :: Poly -> Poly -> Poly
 addPoly (Poly p1) (Poly p2) =
   Poly $ zipWith (+) p1 p2
 
-{- | Evaluate the value of the polynomial at point x.
-   |
-   | evalPoly (toPoly [4, 3, 5, 7]) 0
-   | > 7
-   | evalPoly (toPoly [4, 3, 5, 7]) 1
-   | > 19
-   | evalPoly (toPoly [4, 3, 5, 7]) 2
-   | > 61
-   | evalPoly (toPoly [4, 3, 5, 7]) 3
-   | > 157
-   | evalPoly (toPoly [4, -3, -5, 7]) 3
-   | > 73
-   | evalPoly (toPoly [4, -3, 0, 7]) (-1)
-   | > 0
--}
+-- | Evaluate the value of the polynomial at point x.
+-- |
+-- | evalPoly (toPoly [4, 3, 5, 7]) 0
+-- | > 7
+-- | evalPoly (toPoly [4, 3, 5, 7]) 1
+-- | > 19
+-- | evalPoly (toPoly [4, 3, 5, 7]) 2
+-- | > 61
+-- | evalPoly (toPoly [4, 3, 5, 7]) 3
+-- | > 157
+-- | evalPoly (toPoly [4, -3, -5, 7]) 3
+-- | > 73
+-- | evalPoly (toPoly [4, -3, 0, 7]) (-1)
+-- | > 0
 evalPoly :: Poly -> Int -> Int
 evalPoly (Poly xs) x =
   fst $ foldr evalTerm (0, 0) xs
